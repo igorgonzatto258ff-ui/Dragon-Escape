0d00cff952ef3ea69cf7c0c5a8859a67c856ce5d
Igor Gonzatto <igorgonzatto258ff@gmail.com>
2026-05-19 20:29:23 -0300

    Mudei a input de abertura para NickName

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ nome_maior, maior_pontos, dataJogada = maior_pontuador()
 pygame.init()
 
 while True:
-    nome = input("Informe o Nome do Competidor:")
+    nome = input("NickName:")
     if len(nome) > 0: 
         break
     else:
